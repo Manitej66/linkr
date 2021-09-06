d7d36e1ae1ddea00f201c7167abb8a558c5c4bc9
Manitej66 <pratamaniteja66@gmail.com>
2021-09-06 09:57:17 +0530

    fixed minor bugs

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,36 @@ import { nanoid } from "nanoid";
 export default function Index() {
   const [url, setUrl] = useState("");
   const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleSubmit = async () => {
-    const response = await fetch(`/api/getInfo?url=${url}`);
-    const json = await response.json();
-    const videoId = url.match(
-      /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/
-    );
+    // check if a url is a youtube url or not regex
+    const regex =
+      /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?/;
+    const match = url.match(regex);
 
-    const { data, error } = await supabase
-      .from("links")
-      .insert([{ og_link: url, shorten_link: videoId, data: json }]);
-
-    if (error) alert(error.message);
-    if (data) {
-      router.push(
-        `http://twitter.com/share?text=${json.title}&url=https://linkrrr.vercel.app/${videoId}`
+    if (match) {
+      setLoading(true);
+      const response = await fetch(`/api/getInfo?url=${url}`);
+      const json = await response.json();
+      const videoId = url.match(
+        /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/
       );
-    }
+
+      const { data, error } = await supabase
+        .from("links")
+        .insert([{ og_link: url, shorten_link: videoId[1], data: json }]);
+
+      setLoading(false);
+
+      if (error) alert(error.message);
+      if (data) {
+        router.push(
+          `http://twitter.com/share?text=${json.title}&url=https://linkrrr.vercel.app/${videoId[1]}`
+        );
+      }
+    } else alert("Please enter a valid youtube url");
   };
 
   return (
@@ -48,8 +59,8 @@ export default function Index() {
         onChange={(e) => setUrl(e.target.value)}
         placeholder="Enter YouTube video URL"
       />
-      <button className="btn" onClick={handleSubmit}>
-        Share to twitter
+      <button disabled={loading} className="btn" onClick={handleSubmit}>
+        {loading ? "Loading..." : "Share to twitter"}
       </button>
 
       {data && <p>{data}</p>}
